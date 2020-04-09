fcaf97660f36d0b8169e59a5c27d93a5860a86c3
Armand Kamffer <armand.kamffer@entelect.co.za>
2020-04-09 15:19:43 +0200

    Change to UITableView for intro screen

---
+++ DiscoveryArchitecture/Landing/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  DiscoveryArchitecture
+//
+//  Created by Armand Kamffer on 2020/04/09.
+//  Copyright © 2020 Armand Kamffer. All rights reserved.
+//
+
+import UIKit
+
+protocol Scenario {
+    var title: String { get }
+    var items: [ScenarioItem] { get }
+}
+
+protocol ScenarioItem {
+    var title: String { get }
+    func viewController() -> UIViewController
+}
+
+
+class ViewController: UITableViewController {
+    var scenarios: [Scenario] = [
+        ScenarioOne(),
+        ScenarioTwo(),
+        ScenarioThree()
+    ]
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return scenarios.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = scenarios[indexPath.section].items[indexPath.row].title
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return scenarios[section].title
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let viewController = scenarios[indexPath.section].items[indexPath.row].viewController()
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return scenarios[section].items.count
+    }
+}
