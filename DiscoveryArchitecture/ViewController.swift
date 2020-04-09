803c8bb9eb68db29dc1bc10e2f43e96bdab4975a
Armand Kamffer <armand.kamffer@entelect.co.za>
2020-04-09 08:30:02 +0200

    Initial Commit

---
+++ DiscoveryArchitecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiscoveryArchitecture
+//
+//  Created by Armand Kamffer on 2020/04/09.
+//  Copyright © 2020 Armand Kamffer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
